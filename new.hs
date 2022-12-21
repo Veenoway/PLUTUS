6308e8b503e5cd34bd54db4d725a40eb255eacce
Veeno_Dev <77930262+Veenoway@users.noreply.github.com>
2022-12-21 23:23:15 +0100

    f

--- new.hs
+++ new.hs
@@ -11,10 +11,6 @@ myFirstFunction = logInfo @Haskell.String "Hello, world"
 endpoints :: Contract () EmptySchema T.Text ()
 endpoints = myFirstFunction
 
--- 'mkSchemaDefinitions' doesn't work with 'EmptySchema'
--- (that is, with 0 endpoints) so we define a
--- dummy schema type with 1 endpoint to make it compile.
--- TODO: Repair 'mkSchemaDefinitions'
 type DummySchema = Endpoint "dummy" ()
 
 mkSchemaDefinitions ''DummySchema
